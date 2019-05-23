4935698ed253551edb2f9b29b19331a2dd9e0edf
colinlcrawford <ccolin84@gmail.com>
2019-05-22 22:16:48 -0400

    create repo with working extension

---
+++ src/background.js
@@ -0,0 +1,27 @@
+/* global browserAction, chrome, tabs, browser */
+
+function runMediumBannerRemoverScript (file) {
+  if (chrome) {
+    chrome.tabs.executeScript({ file })
+  } else if (browser) {
+    tabs.executeScript({ file })
+  }
+}
+
+/**
+ * registers a cb with the browser to be run when the
+ * extension icon is clicked
+ */
+function registerOnExtensionIconClicked (cb) {
+  if (chrome) {
+    chrome.browserAction.onClicked.addListener(cb)
+  } else if (browser) {
+    browserAction.onClicked.addListener(cb)
+  }
+}
+
+function removeMediumBanners() {
+  return runMediumBannerRemoverScript('src/remove-medium-banners.js')
+}
+
+registerOnExtensionIconClicked(removeMediumBanners)
---
+++ src/remove-medium-banners.js
@@ -0,0 +1,43 @@
+function getMediumStickyHeader () {
+  return document.querySelector('.metabar.js-metabar')
+}
+
+function getMediumStickyFooter () {
+  return document.querySelector('.js-stickyFooter')
+}
+
+function getMediumBanners () {
+  return filter([
+    getMediumStickyHeader(),
+    getMediumStickyFooter()
+  ], isTruthy)
+}
+
+function removeDomNode (domNode) {
+  return domNode.remove()
+}
+
+/**
+ * removes the medium ad and popup banners from the page
+ */
+function removeMediumBanners () {
+  return map(getMediumBanners(), removeDomNode)
+}
+
+removeMediumBanners()
+
+// ==========================================
+// ===== Some helper functions
+// ==========================================
+function map (arr, fn) {
+  return arr.map(fn)
+}
+
+function filter (arr, fn) {
+  return arr.filter(fn)
+}
+
+function isTruthy (value) {
+  return !!value
+}
+// ==========================================
