e15905976a9f8aca289b2500e8d2038912a8cdf7
colinlcrawford <ccolin84@gmail.com>
2019-10-19 23:55:33 -0400

    refactor banner removal script

--- medium-banner-remover/content-scripts/remove-medium-banners.js
+++ medium-banner-remover/content-scripts/remove-medium-banners.js
@@ -1,3 +1,76 @@
+/* =============================== Util Fns ================================ */
+function map (fn) {
+  return arr => arr.map(fn)
+}
+
+function filter (fn) {
+  return arr => arr.filter(fn)
+}
+
+function reduce (fn) {
+  return arr => initialVal => arr.reduce(fn, initialVal)
+}
+
+function isTruthy (value) {
+  return !!value
+}
+
+function pipe (fns) {
+  return reduce((acc, nextFn) => nextFn(acc))(fns)
+}
+
+function emptyArray (n) {
+  return Array(n).fill()
+}
+
+function first (arr) {
+  return arr[0]
+}
+
+function iF (predicate) {
+  return whenTrue => whenFalse => value => (
+    predicate(value) ? whenTrue(value) : whenFalse(value)
+  )
+}
+
+function when (predicate) {
+  return whenTrue => iF(predicate)(whenTrue)(() => undefined)
+}
+
+function id (value) {
+  return () => value
+}
+/* ========================================================================= */
+
+/* =============================== DOM Utils =============================== */
+function removeDomNode (domNode) {
+  return domNode.remove()
+}
+
+function querySelector (selector) {
+  return document.querySelector(selector)
+}
+
+function querySelectorAll (selector) {
+  return [...document.querySelectorAll(selector)]
+}
+
+function parentNode (domNode) {
+  return domNode.parentNode
+}
+
+function nthGenerationParent (n) {
+  const safelyGetParent = when(isTruthy)(parentNode)
+  const getNParents = map(id(safelyGetParent))(emptyArray(n))
+  return pipe(getNParents)
+}
+
+function hasText (text) {
+  return node => node.textContent.includes(text)
+}
+/* ========================================================================= */
+
+/* ============================ DOM Interactors ============================ */
 function getNav () {
   return querySelector('nav')
 }
@@ -26,9 +99,7 @@ function getBottomBanner () {
 
 function getBottomFreeSignUpExtraPreviewBanner () {
   return pipe([
-    filter(
-      hasText('Get one more story in your member preview when you sign up')
-    ),
+    filter(hasText('Get one more story in your member preview when you sign up')),
     first,
     nthGenerationParent(6)
   ])(querySelectorAll('h2'))
@@ -43,111 +114,14 @@ function getMediumBanners () {
   ])
 }
 
-/**
- * removes the medium ad and popup banners from the page
- */
+/** removes the medium ad and popup banners from the page */
 function removeMediumBanners () {
   return map(removeDomNode)(getMediumBanners())
 }
+/* ========================================================================= */
 
-removeMediumBanners()
-
-/*
-==========================================
-|======== Some helper functions =========|
-==========================================
-*/
-function map (fn) {
-  return function (arr) {
-    return arr.map(fn)
-  }
+function main () {
+  removeMediumBanners()
 }
 
-function filter (fn) {
-  return function (arr) {
-    return arr.filter(fn)
-  }
-}
-
-function reduce (fn) {
-  return function (arr) {
-    return function (initialVal) {
-      return arr.reduce(fn, initialVal)
-    }
-  }
-}
-
-function isTruthy (value) {
-  return !!value
-}
-
-function pipe (fns) {
-  return reduce(function (acc, nextFn) {
-    return nextFn(acc)
-  })(fns)
-}
-
-function emptyArray (n) {
-  return Array(n).fill()
-}
-
-function first (arr) {
-  return arr[0]
-}
-
-function iF (predicate) {
-  return function (whenTrue) {
-    return function (whenFalse) {
-      return function (value) {
-        return predicate(value) ? whenTrue(value) : whenFalse(value)
-      }
-    }
-  }
-}
-
-function when (predicate) {
-  return function (whenTrue) {
-    return iF(predicate)(whenTrue)(function () {
-      return undefined
-    })
-  }
-}
-
-function id (value) {
-  return function () {
-    return value
-  }
-}
-
-/*
- * DOM helpers
- */
-
-function removeDomNode (domNode) {
-  return domNode.remove()
-}
-
-function querySelector (selector) {
-  return document.querySelector(selector)
-}
-
-function querySelectorAll (selector) {
-  return [...document.querySelectorAll(selector)]
-}
-
-function parentNode (domNode) {
-  return domNode.parentNode
-}
-
-function nthGenerationParent (n) {
-  const safelyGetParent = when(isTruthy)(parentNode)
-  const getNParents = map(id(safelyGetParent))(emptyArray(n))
-  return pipe(getNParents)
-}
-
-function hasText (text) {
-  return function (node) {
-    return node.textContent.includes(text)
-  }
-}
-/* ===================================== */
+main()
